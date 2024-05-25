6647fc2a551c8e4022e80312bbc577526e83e0d8
Luca Taschner <iMortalTrix@Proton.me>
2024-05-25 22:33:15 +0200

    Add new config, plugin info, user secrets, and plugin classes for Ticketsystem Plugin
    This commit includes the creation of new configuration file (config.json), plugin information file (plugin.xml), and addition of UserSecretsId in .csproj file for the Ticketsystem Plugin. Moreover, the TicketSystem class now extends the DefaultPlugin class and includes OnLoad() and OnUnload() methods.

--- Ticketsystem Plugin/TicketSystem.cs
+++ Ticketsystem Plugin/TicketSystem.cs
@@ -1,5 +1,14 @@
 ﻿namespace Ticketsystem_Plugin;
 
-public class TicketSystem
+public class TicketSystem: PluginBase.DefaultPlugin
 {
+    public override void OnLoad()
+    {
+        
+    }
+
+    public override void OnUnload()
+    {
+        
+    }
 }
